d1d3be89c2877ec00cf95e9d177aff9720f3f09e
Grace <kanmorning@gmail.com>
2022-11-02 18:21:00 +0800

    layout update

--- src/App.js
+++ src/App.js
@@ -1,11 +1,20 @@
 import "./scss/App.scss";
 import Product from "./components/Product";
+import logo from "../src/images/shopping-cart.svg";
 
 function App() {
   return (
-    <div className="container">
-      3355
-      <Product />
+    <div className="app">
+      <header className="fb">
+        <h1>Order Food</h1>
+        <button className="btn">
+          <img className="shoppingLogo" src={logo} alt="shopping" />
+          <span className="ml10">購物車</span>
+        </button>
+      </header>
+      <div className="container">
+        <Product />
+      </div>
     </div>
   );
 }
