a9e385b4b930ebcf646e7fb67a6dbb9a25f912b9
Grace <kanmorning@gmail.com>
2022-11-03 15:07:04 +0800

    layout update

--- src/components/Product.js
+++ src/components/Product.js
@@ -1,8 +1,49 @@
+import imagePlaceholder from "../images/empty.jpg";
+
 function Product() {
+  const list = [
+    {
+      name: "咔啦雞腿堡XL套餐咔啦雞腿堡XL套餐咔啦雞腿堡XL套餐咔啦雞腿堡XL套餐咔啦雞腿堡XL套餐咔啦雞腿堡XL套餐咔啦雞腿堡XL套餐咔啦雞腿堡XL套餐",
+      price: 199,
+      imageWebp: "/images/kfc/webp/01.webp",
+      imagejpg: "/images/kfc/jpg/01.jpeg",
+    },
+    {
+      name: "咔啦雞腿堡XL",
+      price: 120,
+      imageWebp: null,
+      imagejpg: null,
+    },
+  ];
+
+  const currency = "$";
+
   return (
-    <div className="card">
-      <h1>商品名稱</h1>
-    </div>
+    <>
+      {list.map((item) => {
+        const { name, price, imageWebp, imagejpg } = item;
+        return (
+          <div className="card">
+            <div className="imgBox">
+              <picture>
+                <source
+                  className="img cover"
+                  srcset={imageWebp}
+                  type="image/webp"
+                />
+                <img
+                  className="img cover"
+                  src={imagejpg || imagePlaceholder}
+                  alt="product"
+                />
+              </picture>
+            </div>
+            <h1 className="mv2 t16 ellipsis">{name}</h1>
+            <h1 className="mp0 t16 tg8">{`${currency}${price}`}</h1>
+          </div>
+        );
+      })}
+    </>
   );
 }
 
